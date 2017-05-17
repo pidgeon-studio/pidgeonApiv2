759e9152462a72f728128ebb2e46a8bce51ab353
Sirbu Nicolae-Cezar <sirbunicolaecezar@gmail.com>
2017-05-17 14:46:13 +0300

    Add test controller

--- app/controllers/test_controller.rb
+++ app/controllers/test_controller.rb
@@ -1,15 +1,28 @@
 class TestController < ApplicationController
 
   def index
+    @recipient = '0727686700'
+    @message   = 'Hello from Raspberry PIv2'
 
-
-
-
-
-
-    render json: {
-      test: true
-    }
+    render json: create_file(@recipient, @message)
   end
+
+
+  private
+    def create_file(recipient, message)
+      path    = ENV['SMS_PATH']
+      message = "To: #{ recipient } \n\n#{ message }"
+      timestamp = Time.now.strftime('%S-%M-%H-%Y-%d-%m')
+
+      out_file = File.new("#{ path }/#{ timestamp }-#{ recipient }", 'w')
+      out_file.puts(message)
+      out_file.close
+
+      return {
+        recipient: recipient,
+        message: message,
+        timestamp: timestamp
+      }
+    end
 
 end
